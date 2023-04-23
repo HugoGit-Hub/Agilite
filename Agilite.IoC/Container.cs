1438040fbd12867818992d01795251778772f7e0
Hugo <hugo.decuq.34@icloud.com>
2023-04-23 19:50:35 +0200

    fix IoC

--- Agilite.IoC/Container.cs
+++ Agilite.IoC/Container.cs
@@ -1,60 +1,6 @@
-﻿using Agilite.UI.Services;
-using Agilite.UI.Services.Services;
-using Microsoft.Extensions.DependencyInjection;
-using Refit;
-
-namespace Agilite.UI.IoC;
+﻿namespace Agilite.UI.IoC;
 
 public static class Container
 {
-    public static void ConfigureService(IServiceCollection services)
-    {
-        services
-            .AddRefitClient<IContactService>()
-            .ConfigureHttpClient(c => c.BaseAddress = new Uri(EndPointConstantes.API_ADDRESS));
 
-        services
-            .AddRefitClient<IMessageService>()
-            .ConfigureHttpClient(c => c.BaseAddress = new Uri(EndPointConstantes.API_ADDRESS));
-
-        services
-            .AddRefitClient<IObjectiveService>()
-            .ConfigureHttpClient(c => c.BaseAddress = new Uri(EndPointConstantes.API_ADDRESS));
-
-        services
-            .AddRefitClient<IPlanningService>()
-            .ConfigureHttpClient(c => c.BaseAddress = new Uri(EndPointConstantes.API_ADDRESS));
-
-        services
-            .AddRefitClient<IProjectService>()
-            .ConfigureHttpClient(c => c.BaseAddress = new Uri(EndPointConstantes.API_ADDRESS));
-
-        services
-            .AddRefitClient<ISprintService>()
-            .ConfigureHttpClient(c => c.BaseAddress = new Uri(EndPointConstantes.API_ADDRESS));
-
-        services
-            .AddRefitClient<ITaskService>()
-            .ConfigureHttpClient(c => c.BaseAddress = new Uri(EndPointConstantes.API_ADDRESS));
-
-        services
-            .AddRefitClient<ITeamRoleService>()
-            .ConfigureHttpClient(c => c.BaseAddress = new Uri(EndPointConstantes.API_ADDRESS));
-
-        services
-            .AddRefitClient<ITeamService>()
-            .ConfigureHttpClient(c => c.BaseAddress = new Uri(EndPointConstantes.API_ADDRESS));
-
-        services
-            .AddRefitClient<IUserMessageContactService>()
-            .ConfigureHttpClient(c => c.BaseAddress = new Uri(EndPointConstantes.API_ADDRESS));
-
-        services
-            .AddRefitClient<IUserService>()
-            .ConfigureHttpClient(c => c.BaseAddress = new Uri(EndPointConstantes.API_ADDRESS));
-
-        services
-            .AddRefitClient<IUserTeamTeamRoleService>()
-            .ConfigureHttpClient(c => c.BaseAddress = new Uri(EndPointConstantes.API_ADDRESS));
-    }
 }
--- Agilite.UI/App.xaml.cs
+++ Agilite.UI/App.xaml.cs
@@ -6,84 +6,82 @@ using Refit;
 using System;
 using System.Windows;
 
-namespace Agilite.UI
+namespace Agilite.UI;
+
+public partial class App : Application
 {
-    public partial class App : Application
+    public static IHost? AppHost { get; private set; }
+
+    public App()
     {
-        public static IHost? AppHost { get; private set; }
+        AppHost = Host.CreateDefaultBuilder()
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddSingleton<MainWindow>();
 
-        public App()
-        {
-            AppHost = Host.CreateDefaultBuilder()
-                .ConfigureServices((hostContext, services) =>
-                {
-                    services.AddSingleton<MainWindow>();
+                services
+                    .AddRefitClient<IContactService>()
+                    .ConfigureHttpClient(c => c.BaseAddress = new Uri(EndPointConstantes.API_ADDRESS));
 
-                    services
-                        .AddRefitClient<IContactService>()
-                        .ConfigureHttpClient(c => c.BaseAddress = new Uri(EndPointConstantes.API_ADDRESS));
+                services
+                    .AddRefitClient<IMessageService>()
+                    .ConfigureHttpClient(c => c.BaseAddress = new Uri(EndPointConstantes.API_ADDRESS));
 
-                    services
-                        .AddRefitClient<IMessageService>()
-                        .ConfigureHttpClient(c => c.BaseAddress = new Uri(EndPointConstantes.API_ADDRESS));
+                services
+                    .AddRefitClient<IObjectiveService>()
+                    .ConfigureHttpClient(c => c.BaseAddress = new Uri(EndPointConstantes.API_ADDRESS));
 
-                    services
-                        .AddRefitClient<IObjectiveService>()
-                        .ConfigureHttpClient(c => c.BaseAddress = new Uri(EndPointConstantes.API_ADDRESS));
+                services
+                    .AddRefitClient<IPlanningService>()
+                    .ConfigureHttpClient(c => c.BaseAddress = new Uri(EndPointConstantes.API_ADDRESS));
 
-                    services
-                        .AddRefitClient<IPlanningService>()
-                        .ConfigureHttpClient(c => c.BaseAddress = new Uri(EndPointConstantes.API_ADDRESS));
+                services
+                    .AddRefitClient<IProjectService>()
+                    .ConfigureHttpClient(c => c.BaseAddress = new Uri(EndPointConstantes.API_ADDRESS));
 
-                    services
-                        .AddRefitClient<IProjectService>()
-                        .ConfigureHttpClient(c => c.BaseAddress = new Uri(EndPointConstantes.API_ADDRESS));
+                services
+                    .AddRefitClient<ISprintService>()
+                    .ConfigureHttpClient(c => c.BaseAddress = new Uri(EndPointConstantes.API_ADDRESS));
 
-                    services
-                        .AddRefitClient<ISprintService>()
-                        .ConfigureHttpClient(c => c.BaseAddress = new Uri(EndPointConstantes.API_ADDRESS));
+                services
+                    .AddRefitClient<ITaskService>()
+                    .ConfigureHttpClient(c => c.BaseAddress = new Uri(EndPointConstantes.API_ADDRESS));
 
-                    services
-                        .AddRefitClient<ITaskService>()
-                        .ConfigureHttpClient(c => c.BaseAddress = new Uri(EndPointConstantes.API_ADDRESS));
+                services
+                    .AddRefitClient<ITeamRoleService>()
+                    .ConfigureHttpClient(c => c.BaseAddress = new Uri(EndPointConstantes.API_ADDRESS));
 
-                    services
-                        .AddRefitClient<ITeamRoleService>()
-                        .ConfigureHttpClient(c => c.BaseAddress = new Uri(EndPointConstantes.API_ADDRESS));
+                services
+                    .AddRefitClient<ITeamService>()
+                    .ConfigureHttpClient(c => c.BaseAddress = new Uri(EndPointConstantes.API_ADDRESS));
 
-                    services
-                        .AddRefitClient<ITeamService>()
-                        .ConfigureHttpClient(c => c.BaseAddress = new Uri(EndPointConstantes.API_ADDRESS));
+                services
+                    .AddRefitClient<IUserMessageContactService>()
+                    .ConfigureHttpClient(c => c.BaseAddress = new Uri(EndPointConstantes.API_ADDRESS));
 
-                    services
-                        .AddRefitClient<IUserMessageContactService>()
-                        .ConfigureHttpClient(c => c.BaseAddress = new Uri(EndPointConstantes.API_ADDRESS));
+                services
+                    .AddRefitClient<IUserService>()
+                    .ConfigureHttpClient(c => c.BaseAddress = new Uri(EndPointConstantes.API_ADDRESS));
 
-                    services
-                        .AddRefitClient<IUserService>()
-                        .ConfigureHttpClient(c => c.BaseAddress = new Uri(EndPointConstantes.API_ADDRESS));
+                services
+                    .AddRefitClient<IUserTeamTeamRoleService>()
+                    .ConfigureHttpClient(c => c.BaseAddress = new Uri(EndPointConstantes.API_ADDRESS));
+            }).Build();
+    }
 
-                    services
-                        .AddRefitClient<IUserTeamTeamRoleService>()
-                        .ConfigureHttpClient(c => c.BaseAddress = new Uri(EndPointConstantes.API_ADDRESS));
-                })
-                .Build();
-        }
+    protected override async void OnStartup(StartupEventArgs e)
+    {
+        await AppHost!.StartAsync();
 
-        protected override async void OnStartup(StartupEventArgs e)
-        {
-            await AppHost!.StartAsync();
+        var startupForm = AppHost.Services.GetRequiredService<MainWindow>();
+        startupForm.Show();
 
-            var startupForm = AppHost.Services.GetRequiredService<MainWindow>();
-            startupForm.Show();
+        base.OnStartup(e);
+    }
 
-            base.OnStartup(e);
-        }
-
-        protected override async void OnExit(ExitEventArgs e)
-        {
-            await AppHost!.StopAsync();
-            base.OnExit(e);
-        }
+    protected override async void OnExit(ExitEventArgs e)
+    {
+        await AppHost!.StopAsync();
+        base.OnExit(e);
     }
 }
