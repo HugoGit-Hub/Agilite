12638c1368f86d65b844af0a0fb67565633cf3d7
Hugo <hugo.decuq.34@icloud.com>
2023-07-12 22:05:07 +0200

    Mapping DTO and Model

---
+++ Agilite.UI.Mapper/Profiles/LoginProfile.cs
@@ -0,0 +1,14 @@
+﻿using Agilite.DataTransferObject.DTOs;
+using Agilite.UI.Models.Models;
+using AutoMapper;
+
+namespace Agilite.UI.Mapper.Profiles;
+
+public class LoginProfile : Profile
+{
+    public LoginProfile()
+    {
+        CreateMap<LoginDto, LoginModel>();
+        CreateMap<LoginModel, LoginDto>();
+    }
+}
