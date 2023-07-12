a3c156a8dc31e7422b12cfe37c611a48c72590ff
Hugo <hugo.decuq.34@icloud.com>
2023-07-12 22:04:13 +0200

    Create the LoginCommand and the custom RelayCommand

---
+++ Agilite.UI/ViewModels/Command/RelayCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Agilite.UI.ViewModels.Command;
+
+public abstract class RelayCommand : ICommand
+{
+    public event EventHandler? CanExecuteChanged;
+
+    public virtual bool CanExecute(object? parameter) => true;
+
+    public abstract void Execute(object? parameter);
+
+    protected void OnCanExecutedChanged()
+    {
+        CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
