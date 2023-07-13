2c6b2313344edfe555451963a413dcb0f5f78603
Hugo <hugo.decuq.34@icloud.com>
2023-07-13 20:31:19 +0200

    Create Models

---
+++ Agilite.UI.Models/Models/JobStateModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Agilite.UI.Models.Models;
+
+public class JobStateModel
+{
+    public int IdJobState { get; set; }
+
+    public string NameJobState { get; set; } = null!;
+}
---
+++ Agilite.UI.Models/Models/ObjectiveTypeModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Agilite.UI.Models.Models;
+
+public class ObjectiveTypeModel
+{
+    public int IdObjectiveType { get; set; }
+
+    public string NameObjectiveType { get; set; } = null!;
+}
