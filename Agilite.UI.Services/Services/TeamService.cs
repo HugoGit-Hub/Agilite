4f631d8c1db8fcee5257fb66b2270b28fdaf9581
Hugo DECUQ <hugo.decuq@genapi.fr>
2023-07-19 08:03:03 +0200

    Change background color

---
+++ Agilite.UI.Services/Services/TeamService.cs
@@ -0,0 +1,32 @@
+﻿using Agilite.DataTransferObject.DTOs;
+using Agilite.UI.Services.Services.Refit;
+
+namespace Agilite.UI.Services.Services;
+
+public class TeamService : ITeamService
+{
+    public TeamDto Create(TeamDto entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public TeamDto Update(TeamDto entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<TeamDto>> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public TeamDto Get(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public TeamDto Delete(TeamDto entity)
+    {
+        throw new NotImplementedException();
+    }
+}
