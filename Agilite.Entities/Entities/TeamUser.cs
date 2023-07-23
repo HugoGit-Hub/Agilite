d8da61c931f6ae67a234e4d28adba922f318549b
Hugo DECUQ <hugo.decuq@genapi.fr>
2023-07-23 23:21:45 +0200

    Create Entity

---
+++ Agilite.Entities/Entities/TeamUser.cs
@@ -0,0 +1,10 @@
+﻿namespace Agilite.Entities.Entities;
+
+public class TeamUser
+{
+    public int TeamIdTeam { get; set; }
+    public int UserIdUser { get; set; }
+    
+    public Team Team { get; set; }
+    public User User { get; set; }
+}
