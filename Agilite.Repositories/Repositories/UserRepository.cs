2dbd77be55036fe5949f42da4cb5019ddb105651
Hugo DECUQ <hugo.decuq@genapi.fr>
2023-07-26 08:33:29 +0200

    Create Repository GetAllTeamsOfOneUser

--- Agilite.Repositories/Repositories/UserRepository.cs
+++ Agilite.Repositories/Repositories/UserRepository.cs
@@ -7,6 +7,7 @@ namespace Agilite.Repositories.Repositories;
 public interface IUserRepository
 {
     public Task<User> GetUserByEmail(string email, CancellationToken cancellationToken);
+    public Task<IEnumerable<Team>> GetAllTeamsOfOneUser(int idUser, CancellationToken cancellationToken);
 }
 
 public class UserRepository : IUserRepository
@@ -14,10 +15,13 @@ public class UserRepository : IUserRepository
     private readonly AgiliteContext _context;
 
     public UserRepository(AgiliteContext context)
-    {
-        _context = context;
-    }
+        => _context = context;
 
     public async Task<User> GetUserByEmail(string email, CancellationToken cancellationToken)
         => await _context.Users.FirstAsync(user => user.EmailUser == email, cancellationToken);
+
+    public async Task<IEnumerable<Team>> GetAllTeamsOfOneUser(int idUser, CancellationToken cancellationToken)
+        => await _context.Teams
+            .Where(e => e.Users.Any(user => user.IdUser == idUser))
+            .ToListAsync(cancellationToken);
 }
