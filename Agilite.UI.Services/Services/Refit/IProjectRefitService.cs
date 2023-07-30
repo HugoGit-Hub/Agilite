ae81a2061af55b03682e29e78893dc14b22a704a
Hugo DECUQ <hugo.decuq@genapi.fr>
2023-07-30 20:43:26 +0200

    Services

---
+++ Agilite.UI.Services/Services/Refit/IProjectRefitService.cs
@@ -0,0 +1,25 @@
+﻿using Agilite.DataTransferObject.DTOs;
+using Refit;
+
+namespace Agilite.UI.Services.Services.Refit;
+
+public interface IProjectRefitService : IBaseRefitClient
+{
+    [Post(EndPointConstantes.CREATE_PROJECT)]
+    public Task<ProjectDto> Create(ProjectDto entity);
+
+    [Put(EndPointConstantes.UPDATE_PROJECT)]
+    public Task<ProjectDto> Update(ProjectDto entity);
+
+    [Get(EndPointConstantes.GET_ALL_PROJECTS)]
+    public Task<IEnumerable<ProjectDto>> GetAll();
+
+    [Get(EndPointConstantes.GET_ALL_PROJECTS_OF_ONE_TEAM)]
+    public Task<IEnumerable<ProjectDto>> GetAllProjectsOfOneTeam(int idTeam);
+
+    [Get(EndPointConstantes.GET_PROJECT)]
+    public Task<ProjectDto> Get(int id);
+
+    [Delete(EndPointConstantes.DELETE_PROJECT)]
+    public Task<ProjectDto> Delete(ProjectDto entity);
+}
