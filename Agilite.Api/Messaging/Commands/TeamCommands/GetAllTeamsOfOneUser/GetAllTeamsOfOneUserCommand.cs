b924bff69718c5231b577208e84b5846fc89bb99
Hugo DECUQ <hugo.decuq@genapi.fr>
2023-07-31 20:48:11 +0200

    Add project Messaging

---
+++ Agilite.Api/Messaging/Commands/TeamCommands/GetAllTeamsOfOneUser/GetAllTeamsOfOneUserCommand.cs
@@ -0,0 +1,6 @@
+﻿using Agilite.DataTransferObject.DTOs;
+using MediatR;
+
+namespace Agilite.Api.Messaging.Commands.TeamCommands.GetAllTeamsOfOneUser;
+
+public record GetAllTeamsOfOneUserCommand(int IdUser) : IRequest<IEnumerable<TeamDto>>;
---
+++ Agilite.Api/Messaging/Commands/TeamCommands/GetAllTeamsOfOneUser/GetAllTeamsOfOneUserCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using Agilite.DataTransferObject.DTOs;
+using Agilite.Services;
+using AutoMapper;
+using MediatR;
+
+namespace Agilite.Api.Messaging.Commands.TeamCommands.GetAllTeamsOfOneUser;
+
+public class GetAllTeamsOfOneUserCommandHandler : IRequestHandler<GetAllTeamsOfOneUserCommand, IEnumerable<TeamDto>>
+{
+    private readonly IMapper _mapper;
+    private readonly ITeamService _service;
+
+    public GetAllTeamsOfOneUserCommandHandler(IMapper mapper, ITeamService service)
+    {
+        _mapper = mapper;
+        _service = service;
+    }
+
+    public async Task<IEnumerable<TeamDto>> Handle(GetAllTeamsOfOneUserCommand request, CancellationToken cancellationToken)
+    {
+        var result = await _service.GetAllTeamsOfOneUser(request.IdUser, cancellationToken);
+        return _mapper.Map<IEnumerable<TeamDto>>(result);
+    }
+}
