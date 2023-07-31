3e55a0bce092decadfb2b63f4e444b1462360810
Hugo DECUQ <hugo.decuq@genapi.fr>
2023-07-31 20:29:21 +0200

    Repository

---
+++ Agilite.Repositories/Repositories/SprintRepository.cs
@@ -0,0 +1,24 @@
+﻿using Agilite.Entities.Entities;
+using Agilite.UnitOfWork.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Agilite.Repositories.Repositories;
+
+public interface ISprintRepository
+{
+    public Task<List<Sprint>> GetAllSprintsOfOneProject(int idProject, CancellationToken cancellationToken);
+}
+
+public class SprintRepository : ISprintRepository
+{
+    private readonly AgiliteContext _context;
+
+    public SprintRepository(AgiliteContext context)
+        => _context = context;
+
+    public async Task<List<Sprint>> GetAllSprintsOfOneProject(int idProject, CancellationToken cancellationToken)
+        => await _context.Projects
+            .Where(e => e.IdProject == idProject)
+            .SelectMany(e => e.Sprints)
+            .ToListAsync(cancellationToken);
+}
