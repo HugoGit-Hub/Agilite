0363fa098b406864e4daae38eabc8882df9b80da
Hugo DECUQ <hugo.decuq@genapi.fr>
2023-07-31 20:28:38 +0200

    Messaging

---
+++ Agilite.Api/Messaging/Commands/SprintComands/GetAllSprintsOfOneProject/GetAllSprintsOfOneProjectCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using Agilite.DataTransferObject.DTOs;
+using Agilite.Services;
+using AutoMapper;
+using MediatR;
+
+namespace Agilite.Api.Messaging.Commands.SprintComands.GetAllSprintsOfOneProject;
+
+public class GetAllSprintsOfOneProjectCommandHandler : IRequestHandler<GetAllSprintsOfOneProjectCommand, IEnumerable<SprintDto>>
+{
+    private readonly IMapper _mapper;
+    private readonly ISprintService _service;
+
+    public GetAllSprintsOfOneProjectCommandHandler(IMapper mapper, ISprintService service)
+    {
+        _mapper = mapper;
+        _service = service;
+    }
+
+    public async Task<IEnumerable<SprintDto>> Handle(GetAllSprintsOfOneProjectCommand request, CancellationToken cancellationToken)
+    {
+        var result = await _service.GetAllSprintsOfOneProject(request.IdProject, cancellationToken);
+        return _mapper.Map<IEnumerable<SprintDto>>(result);
+    }
+}
