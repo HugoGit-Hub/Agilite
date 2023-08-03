1fc8147d9020db4b29ed7434610d898c5a7ff06c
Hugo DECUQ <hugo.decuq@genapi.fr>
2023-08-03 19:05:04 +0200

    Clear Team view and view model

--- Agilite.UI/ViewModels/TeamViewModel.cs
+++ Agilite.UI/ViewModels/TeamViewModel.cs
@@ -1,7 +1,9 @@
-﻿using Agilite.UI.Models.Models;
+﻿using Agilite.UI.Message;
+using Agilite.UI.Models.Models;
 using Agilite.UI.Services.Services;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
 
@@ -14,6 +16,7 @@ public class TeamViewModel : ObservableObject
     private readonly ITeamService _teamService;
     private readonly IProjectService _projectService;
     private readonly ICommand _getAllProjectsOfOneTeamCommand;
+    private readonly ICommand _showSprintsButtons;
 
     public ICommand GetAllProjectsOfOneTeamCommand
     {
@@ -21,15 +24,26 @@ public class TeamViewModel : ObservableObject
         private init => SetProperty(ref _getAllProjectsOfOneTeamCommand, value);
     }
 
+    public ICommand ShowSprintsButtonsCommand
+    {
+        get => _showSprintsButtons;
+        private init => SetProperty(ref _showSprintsButtons, value);
+    }
+
     public ObservableCollection<TeamModel> Teams { get; } = new();
     public ObservableCollection<ProjectModel> Projects { get; } = new();
 
-    public TeamViewModel(ITeamService teamService, IProjectService projectService)
+    public TeamViewModel(
+        ITeamService teamService,
+        IProjectService projectService)
     {
         _teamService = teamService;
         _projectService = projectService;
+
         GetAllTeamsOfOneUser(int.Parse(TokenService.GetClaimValue(ID_USER)));
+
         GetAllProjectsOfOneTeamCommand = new RelayCommand<int>(GetAllProjectsOfOneTeam);
+        ShowSprintsButtonsCommand = new RelayCommand(ShowSprintsButtonMessage);
     }
 
     private async void GetAllTeamsOfOneUser(int id)
@@ -53,4 +67,7 @@ public class TeamViewModel : ObservableObject
             Projects.Add(project);
         }
     }
+
+    private static void ShowSprintsButtonMessage()
+        => WeakReferenceMessenger.Default.Send(new ShowSprintsButtonsMessage(true));
 }
