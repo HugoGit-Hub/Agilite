92463dd5606bce56ce0ea314d57a566879302e88
Hugo DECUQ <hugo.decuq@genapi.fr>
2023-08-03 19:04:39 +0200

    Clear MainWindow view and view model

--- Agilite.UI/ViewModels/MainWindowViewModel.cs
+++ Agilite.UI/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,8 @@
-﻿using Agilite.UI.Views;
+﻿using Agilite.UI.Message;
+using Agilite.UI.Views;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
 using System;
 using System.Windows.Input;
 
@@ -8,22 +10,41 @@ namespace Agilite.UI.ViewModels;
 
 public class MainWindowViewModel : ObservableObject
 {
-    public static string DEFAULT_VIEW => nameof(DefaultView);
-
-    public static string TEAM_VIEW => nameof(TeamView);
-
     private readonly DefaultViewModel _defaultViewModel;
     private readonly TeamViewModel _teamViewModel;
+    private readonly ICommand _toggleSprintsButtons;
+
+    private bool _showSprints;
+
+    public static string DEFAULT_VIEW => nameof(DefaultView);
+    public static string TEAM_VIEW => nameof(TeamView);
 
     public ObservableObject ActualPage { get; private set; }
-
     public ICommand ChangeViewCommand { get; }
 
-    public MainWindowViewModel(
-        DefaultViewModel defaultViewModel,
-        TeamViewModel teamViewModel)
+    public bool ShowSprints
+    {
+        get => _showSprints;
+        set
+        {
+            if (_showSprints == value) return;
+            _showSprints = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public ICommand ToggleSprintsButtonsCommand
+    {
+        get => _toggleSprintsButtons;
+        private init => SetProperty(ref _toggleSprintsButtons, value);
+    }
+
+    public MainWindowViewModel(DefaultViewModel defaultViewModel, TeamViewModel teamViewModel)
     {
         ChangeViewCommand = new RelayCommand<string>(SwitchView);
+        ToggleSprintsButtonsCommand = new RelayCommand(ToggleSprintsButtons);
+
+        WeakReferenceMessenger.Default.Register<ShowSprintsButtonsMessage>(this, ShowSprintsButtons);
 
         ActualPage = defaultViewModel;
 
@@ -42,5 +63,13 @@ public class MainWindowViewModel : ObservableObject
         };
 
         OnPropertyChanged(nameof(ActualPage));
+    }
+
+    private void ShowSprintsButtons(object recipient, ShowSprintsButtonsMessage message)
+        => ShowSprints = message.Value;
+
+    private void ToggleSprintsButtons()
+    {
+        var _ = ShowSprints ? ShowSprints = false : ShowSprints = true;
     }
 }
