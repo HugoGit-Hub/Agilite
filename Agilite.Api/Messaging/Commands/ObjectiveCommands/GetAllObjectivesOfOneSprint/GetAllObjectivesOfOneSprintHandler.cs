9cade1ec98492056c1a383498fe472899a7edd2c
Hugo DECUQ <hugo.decuq@genapi.fr>
2023-08-04 19:12:12 +0200

    Messaging

---
+++ Agilite.Api/Messaging/Commands/ObjectiveCommands/GetAllObjectivesOfOneSprint/GetAllObjectivesOfOneSprintHandler.cs
@@ -0,0 +1,24 @@
+﻿using Agilite.DataTransferObject.DTOs;
+using Agilite.Services;
+using AutoMapper;
+using MediatR;
+
+namespace Agilite.Api.Messaging.Commands.ObjectiveCommands.GetAllObjectivesOfOneSprint;
+
+public class GetAllObjectivesOfOneSprintHandler : IRequestHandler<GetAllObjectivesOfOneSprintCommand, IEnumerable<ObjectiveDto>>
+{
+    private readonly IMapper _mapper;
+    private readonly IObjectiveService _service;
+
+    public GetAllObjectivesOfOneSprintHandler(IMapper mapper, IObjectiveService service)
+    {
+        _mapper = mapper;
+        _service = service;
+    }
+
+    public async Task<IEnumerable<ObjectiveDto>> Handle(GetAllObjectivesOfOneSprintCommand request, CancellationToken cancellationToken)
+    {
+        var result = await _service.GetAllObjectivesOfOneSprint(request.IdSprint);
+        return _mapper.Map<IEnumerable<ObjectiveDto>>(result);
+    }
+}
