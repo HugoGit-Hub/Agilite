fdb1829bd1ff4770c2e82702645d6bf8db14b66b
Hugo DECUQ <hugo.decuq@genapi.fr>
2023-08-04 19:52:08 +0200

    Create JobRepository

---
+++ Agilite.Repositories/Repositories/JobRepository.cs
@@ -0,0 +1,23 @@
+﻿using Agilite.Entities.Entities;
+using Agilite.UnitOfWork.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Agilite.Repositories.Repositories;
+
+public interface IJobRepository
+{
+    public Task<IEnumerable<Job>> GetAllJobsOfOneObjective(int id, CancellationToken cancellationToken);
+}
+
+public class JobRepository : IJobRepository
+{
+    private readonly AgiliteContext _context;
+
+    public JobRepository(AgiliteContext context)
+        => _context = context;
+
+    public async Task<IEnumerable<Job>> GetAllJobsOfOneObjective(int id, CancellationToken cancellationToken)
+        => await _context.Jobs
+            .Where(x => x.Objectives.Any(objective => objective.IdObjective == id))
+            .ToListAsync(cancellationToken);
+}
