7b86958bcccbfe5b2cfb2798be384fc3e2e64b42
Hugo DECUQ <hugo.decuq@genapi.fr>
2023-08-04 19:50:42 +0200

    Create Job Messages

---
+++ Agilite.Api/Messaging/Commands/JobCommands/GetAllJobsOfOneObjective/GetAllJobsOfOneObjectiveCommand.cs
@@ -0,0 +1,6 @@
+﻿using Agilite.DataTransferObject.DTOs;
+using MediatR;
+
+namespace Agilite.Api.Messaging.Commands.JobCommands.GetAllJobsOfOneObjective;
+
+public record GetAllJobsOfOneObjectiveCommand(int Id) : IRequest<IEnumerable<JobDto>>;
---
+++ Agilite.Api/Messaging/Commands/JobCommands/GetAllJobsOfOneObjective/GetAllJobsOfOneObjectiveCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using Agilite.DataTransferObject.DTOs;
+using Agilite.Services;
+using AutoMapper;
+using MediatR;
+
+namespace Agilite.Api.Messaging.Commands.JobCommands.GetAllJobsOfOneObjective;
+
+public class GetAllJobsOfOneObjectiveCommandHandler : IRequestHandler<GetAllJobsOfOneObjectiveCommand, IEnumerable<JobDto>>
+{
+    private readonly IMapper _mapper;
+    private readonly IJobService _service;
+
+    public GetAllJobsOfOneObjectiveCommandHandler(IMapper mapper, IJobService service)
+    {
+        _mapper = mapper;
+        _service = service;
+    }
+
+    public async Task<IEnumerable<JobDto>> Handle(GetAllJobsOfOneObjectiveCommand request, CancellationToken cancellationToken)
+    {
+        var result = await _service.GetAllJobsOfOneObjective(request.Id, cancellationToken);
+        return _mapper.Map<IEnumerable<JobDto>>(result);
+    }
+}
