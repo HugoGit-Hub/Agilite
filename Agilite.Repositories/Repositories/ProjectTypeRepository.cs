11fe32257b82651c3b697e0f22812dfd26a07c54
Hugo DECUQ <hugo.decuq@genapi.fr>
2023-08-10 09:08:41 +0200

    create reposiotry and service

---
+++ Agilite.Repositories/Repositories/ProjectTypeRepository.cs
@@ -0,0 +1,21 @@
+﻿using Agilite.Entities.Entities;
+using Agilite.UnitOfWork.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Agilite.Repositories.Repositories;
+
+public interface IProjectTypeRepository
+{
+    public Task<IEnumerable<ProjectType>> GetAllProjectType(CancellationToken cancellationToken);
+}
+
+public class ProjectTypeRepository : IProjectTypeRepository
+{
+    private readonly AgiliteContext _context;
+
+    public ProjectTypeRepository(AgiliteContext context)
+        => _context = context;
+
+    public async Task<IEnumerable<ProjectType>> GetAllProjectType(CancellationToken cancellationToken)
+        => await _context.ProjectTypes.ToListAsync(cancellationToken);
+}
---
+++ Agilite.Services/ProjectTypeService.cs
@@ -0,0 +1,20 @@
+﻿using Agilite.Entities.Entities;
+using Agilite.Repositories.Repositories;
+
+namespace Agilite.Services;
+
+public interface IProjectTypeService
+{
+    public Task<IEnumerable<ProjectType>> GetAllProjectType(CancellationToken cancellationToken);
+}
+
+public class ProjectTypeService : IProjectTypeService
+{
+    private readonly IProjectTypeRepository _repository;
+
+    public ProjectTypeService(IProjectTypeRepository repository)
+        => _repository = repository;
+
+    public async Task<IEnumerable<ProjectType>> GetAllProjectType(CancellationToken cancellationToken)
+        => await _repository.GetAllProjectType(cancellationToken);
+}
