697ac8d62584727aaf2786c6008d52bf7cfbac57
Hugo DECUQ <hugo.decuq@genapi.fr>
2023-08-15 13:22:17 +0200

    Change team dto

--- Agilite.DataTransferObject/DTOs/SprintDto.cs
+++ Agilite.DataTransferObject/DTOs/SprintDto.cs
@@ -4,6 +4,8 @@ public class SprintDto
 {
     public int IdSprint { get; set; }
 
+    public int FkProject { get; set; }
+
     public int NumberSprint { get; set; }
 
     public DateTime StartDateSprint { get; set; }
