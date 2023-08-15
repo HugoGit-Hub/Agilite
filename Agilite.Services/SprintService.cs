3b140c9a33cb221fd39682627ac078dabcd5b346
Hugo DECUQ <hugo.decuq@genapi.fr>
2023-08-15 13:29:57 +0200

    Create team service server side

--- Agilite.Services/SprintService.cs
+++ Agilite.Services/SprintService.cs
@@ -5,6 +5,7 @@ namespace Agilite.Services;
 
 public interface ISprintService
 {
+    public Task<Sprint> Create(Sprint sprint, CancellationToken cancellationToken);
     public Task<IEnumerable<Sprint>> GetAllSprintsOfOneProject(int projectId, CancellationToken cancellationToken);
 }
 
@@ -15,6 +16,19 @@ public class SprintService : ISprintService
     public SprintService(ISprintRepository repository)
         => _repository = repository;
 
+    public async Task<Sprint> Create(Sprint sprint, CancellationToken cancellationToken)
+    {
+        var lastSprint = await GetLastSprintNumber(sprint.FkProject, cancellationToken);
+        sprint.NumberSprint = lastSprint is null ? 1 : lastSprint.Value + 1;
+        sprint.StartDateSprint = DateTime.Now;
+        sprint.EndDateSprint = DateTime.Now.AddDays(14);
+
+        return await _repository.Create(sprint, cancellationToken);
+    }
+
     public async Task<IEnumerable<Sprint>> GetAllSprintsOfOneProject(int projectId, CancellationToken cancellationToken)
         => await _repository.GetAllSprintsOfOneProject(projectId, cancellationToken);
+
+    private async Task<int?> GetLastSprintNumber(int idProject, CancellationToken cancellationToken)
+        => await _repository.GetLastSprintNumber(idProject, cancellationToken);
 }
